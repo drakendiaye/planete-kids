928f49f86aaaac0d86db218de2901751e8e55a8e
guillaume.missonnier <guillaume.missonnier@0f99be20-ee39-0410-a9d1-2d65dd7db8f0>
2007-10-02 13:23:08 +0000

    Implantation du questionnaire

---
+++ branches/version00/src/ecom/beans/CartLocal.java
@@ -0,0 +1,13 @@
+/**
+* eCommerce Application Sample for J2EE Training 
+* Local interface definition for the Cart bean
+* EJB3.0 
+* @author Fabienne Boyer - Didier Donsez - may 2006
+*/
+
+package ecom.beans;
+
+public interface CartLocal {
+
+ // to be completed
+}
