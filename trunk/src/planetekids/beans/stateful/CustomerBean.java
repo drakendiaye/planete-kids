4a9bbf22ba7b57b3415ee0ba5cf5adde78d96ce9
guillaume.missonnier <guillaume.missonnier@0f99be20-ee39-0410-a9d1-2d65dd7db8f0>
2007-11-04 16:10:19 +0000

    correction de problèmes d'accents

--- trunk/src/planetekids/beans/stateful/CustomerBean.java
+++ trunk/src/planetekids/beans/stateful/CustomerBean.java
@@ -22,7 +22,7 @@ public class CustomerBean implements CustomerRemote{
     
     public void init() {
         QuestionnaireBean questionnaire = new QuestionnaireBean(new LocaleBean("Planete Kids ouvre bientot ses portes sur le net.", "Planete Kids open its doors on the web soon."),
-                              new LocaleBean("Aidez-nous Ã  crÃ©er un site Ã  votre image!!!", "Help us to create a site that feets you!!!"));
+                              new LocaleBean("Aidez-nous à  créer un site à  votre image!!!", "Help us to create a site that feets you!!!"));
         
         QuestionBean question = new QuestionBean(questionnaire, new LocaleBean("Combien d'enfants de moins de 17 ans vivent dans votre foyer?", "How many children under 17 live in your home?"), QuestionBean.Pattern.VALUE, 1);
         AnswerBean answer = new AnswerBean(question, new LocaleBean("enfant(s)", "child(ren)"), false, 1);
@@ -32,11 +32,11 @@ public class CustomerBean implements CustomerRemote{
         question = new QuestionBean(questionnaire, new LocaleBean("Quel type de connexion internet utilisez-vous?", "What kind of internet connection do you use?"), QuestionBean.Pattern.SINGLE_CHOICE, 2);
         answer = new AnswerBean(question, new LocaleBean("Connexion par modem", "Modem connection"), false, 1);
         question.getAnswers().add(answer);
-        answer = new AnswerBean(question, new LocaleBean("Connexion haut dÃ©bit", "High band width connection"), false, 2);
+        answer = new AnswerBean(question, new LocaleBean("Connexion haut débit", "High band width connection"), false, 2);
         question.getAnswers().add(answer);
-        answer = new AnswerBean(question, new LocaleBean("Connexion par cÃ¢ble", "Cable connection"), false, 3);
+        answer = new AnswerBean(question, new LocaleBean("Connexion par câble", "Cable connection"), false, 3);
         question.getAnswers().add(answer);
-        answer = new AnswerBean(question, new LocaleBean("Autres, prÃ©cisez :", "Other, precise :"), true, 4);
+        answer = new AnswerBean(question, new LocaleBean("Autres, précisez :", "Other, precise :"), true, 4);
         question.getAnswers().add(answer);
         questionnaire.getQuestions().add(question);
         
@@ -49,7 +49,7 @@ public class CustomerBean implements CustomerRemote{
         question.getAnswers().add(answer);
         answer = new AnswerBean(question, new LocaleBean("Safari", "Safari"), false, 4);
         question.getAnswers().add(answer);
-        answer = new AnswerBean(question, new LocaleBean("Autres, prÃ©cisez :", "Other, precise :"), true, 5);
+        answer = new AnswerBean(question, new LocaleBean("Autres, précisez :", "Other, precise :"), true, 5);
         question.getAnswers().add(answer);
         questionnaire.getQuestions().add(question);
         
@@ -62,18 +62,18 @@ public class CustomerBean implements CustomerRemote{
         question.getAnswers().add(answer);
         questionnaire.getQuestions().add(question);
         
-        question = new QuestionBean(questionnaire, new LocaleBean("D'une maniÃ¨re gÃ©nÃ©rale, lorsque vous acheter sur internet, vous Ãªtes sensible Ã  :", "Usually, when you buy on the internet, you are sensible to :"), QuestionBean.Pattern.SORT, 5);
-        answer = new AnswerBean(question, new LocaleBean("L'attractivitÃ© des prix", "Prices attractivity"), false, 1);
+        question = new QuestionBean(questionnaire, new LocaleBean("D'une manière générale, lorsque vous acheter sur internet, vous êtes sensible à  :", "Usually, when you buy on the internet, you are sensible to :"), QuestionBean.Pattern.SORT, 5);
+        answer = new AnswerBean(question, new LocaleBean("L'attractivité des prix", "Prices attractivity"), false, 1);
         question.getAnswers().add(answer);
-        answer = new AnswerBean(question, new LocaleBean("DiversitÃ© des produits proposÃ©s", "Products diversity"), false, 2);
+        answer = new AnswerBean(question, new LocaleBean("Diversité des produits proposés", "Products diversity"), false, 2);
         question.getAnswers().add(answer);
-        answer = new AnswerBean(question, new LocaleBean("La qualitÃ© du site (EsthÃ©tisme, facilitÃ© d'tilisation...)", "The site quality (Esthetisme, easy to use...)"), false, 3);
+        answer = new AnswerBean(question, new LocaleBean("La qualité du site (Esthétisme, facilité d'tilisation...)", "The site quality (Esthetisme, easy to use...)"), false, 3);
         question.getAnswers().add(answer);
-        answer = new AnswerBean(question, new LocaleBean("Les services proposÃ©s (Garanties, Remboursement...)", "The services (Garanty, Remboursement...)"), false, 4);
+        answer = new AnswerBean(question, new LocaleBean("Les services proposés (Garanties, Remboursement...)", "The services (Garanty, Remboursement...)"), false, 4);
         question.getAnswers().add(answer);
         questionnaire.getQuestions().add(question);
         
-        question = new QuestionBean(questionnaire, new LocaleBean("Dans lesquelles des catÃ©gories suivantes avez-vous acheter un produit ou un service au cours des 12 derniers mois?", "In which of these categories did you buy a product or a service during the past 12 month?"), QuestionBean.Pattern.MULTI_CHOICE, 6);
+        question = new QuestionBean(questionnaire, new LocaleBean("Dans lesquelles des catégories suivantes avez-vous acheter un produit ou un service au cours des 12 derniers mois?", "In which of these categories did you buy a product or a service during the past 12 month?"), QuestionBean.Pattern.MULTI_CHOICE, 6);
         answer = new AnswerBean(question, new LocaleBean("Produits culturels", "Cultural product"), false, 1);
         question.getAnswers().add(answer);
         answer = new AnswerBean(question, new LocaleBean("Informatique", "Computing"), false, 2);
@@ -90,32 +90,32 @@ public class CustomerBean implements CustomerRemote{
         question.getAnswers().add(answer);
         questionnaire.getQuestions().add(question);
         
-        question = new QuestionBean(questionnaire, new LocaleBean("Seriez-vous interessÃ©s par l'achat de vÃ¨tement en ligne?", "Are you interresting in buying clothes on the web?"), QuestionBean.Pattern.SINGLE_CHOICE, 7);
+        question = new QuestionBean(questionnaire, new LocaleBean("Seriez-vous interessés par l'achat de vètement en ligne?", "Are you interresting in buying clothes on the web?"), QuestionBean.Pattern.SINGLE_CHOICE, 7);
         answer = new AnswerBean(question, new LocaleBean("Oui", "Yes"), false, 1);
         question.getAnswers().add(answer);
         answer = new AnswerBean(question, new LocaleBean("Non", "No"), false, 2);
         question.getAnswers().add(answer);
         questionnaire.getQuestions().add(question);
         
-        question = new QuestionBean(questionnaire, new LocaleBean("A quelle frÃ©quence achetez-vous des vÃ¨tements pour vos enfants?", "How often do you buy clothes for your children?"), QuestionBean.Pattern.SINGLE_CHOICE, 8);
+        question = new QuestionBean(questionnaire, new LocaleBean("A quelle fréquence achetez-vous des vètements pour vos enfants?", "How often do you buy clothes for your children?"), QuestionBean.Pattern.SINGLE_CHOICE, 8);
         answer = new AnswerBean(question, new LocaleBean("Une fois par ans", "Once a years"), false, 1);
         question.getAnswers().add(answer);
-        answer = new AnswerBean(question, new LocaleBean("2 Ã  3 fois par ans", "Twice or three times a year"), false, 2);
+        answer = new AnswerBean(question, new LocaleBean("2 à  3 fois par ans", "Twice or three times a year"), false, 2);
         question.getAnswers().add(answer);
-        answer = new AnswerBean(question, new LocaleBean("Quatre fois par ans ou plus, prÃ©cisez :", "four times a year or more, precise :"), true, 3);
+        answer = new AnswerBean(question, new LocaleBean("Quatre fois par ans ou plus, précisez :", "four times a year or more, precise :"), true, 3);
         question.getAnswers().add(answer);
         questionnaire.getQuestions().add(question);
         
-        question = new QuestionBean(questionnaire, new LocaleBean("Qui choisi les vÃ¨tements pour vos enfants?", "Who choose the clothes for your children?"), QuestionBean.Pattern.SINGLE_CHOICE, 9);
+        question = new QuestionBean(questionnaire, new LocaleBean("Qui choisi les vètements pour vos enfants?", "Who choose the clothes for your children?"), QuestionBean.Pattern.SINGLE_CHOICE, 9);
         answer = new AnswerBean(question, new LocaleBean("Vous", "You"), false, 1);
         question.getAnswers().add(answer);
         answer = new AnswerBean(question, new LocaleBean("Vos enfants", "Your children"), false, 2);
         question.getAnswers().add(answer);
-        answer = new AnswerBean(question, new LocaleBean("Autre, prÃ©cisez :", "Other, precise :"), true, 3);
+        answer = new AnswerBean(question, new LocaleBean("Autre, précisez :", "Other, precise :"), true, 3);
         question.getAnswers().add(answer);
         questionnaire.getQuestions().add(question);
         
-        question = new QuestionBean(questionnaire, new LocaleBean("Quel est votre budget annuel pour acheter des vÃ¨tements Ã  vos enfants?", "How much do you spend a year to buy clothes to your children?"), QuestionBean.Pattern.VALUE, 10);
+        question = new QuestionBean(questionnaire, new LocaleBean("Quel est votre budget annuel pour acheter des vètements à  vos enfants?", "How much do you spend a year to buy clothes to your children?"), QuestionBean.Pattern.VALUE, 10);
         answer = new AnswerBean(question, new LocaleBean("euros par an", "euro per year"), false, 1);
         question.getAnswers().add(answer);
         questionnaire.getQuestions().add(question);
--- trunk/web/index.jsp
+++ trunk/web/index.jsp
@@ -3,6 +3,7 @@
 
 <html>
     <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Planete-kids</title>
         <s:head theme="ajax" debug="true" />
         <link href="<s:url value='/style.css'/>" rel="stylesheet" type="text/css"/>
