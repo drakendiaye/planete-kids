0b92a510cca8860858004a161bcfa12806a10f32
clemplantier@gmail.com <clemplantier@gmail.com@0f99be20-ee39-0410-a9d1-2d65dd7db8f0>
2007-12-12 10:21:47 +0000

    Création du formulaire de création de compte (à moitié fait)

---
+++ trunk/src/planetekids/actions/account/CreateAction.java
@@ -0,0 +1,145 @@
+package planetekids.actions.account;
+
+import com.opensymphony.xwork2.ActionSupport;import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
+;
+import com.opensymphony.xwork2.validator.annotations.Validation;
+import java.util.Map;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.apache.struts2.interceptor.SessionAware;
+import planetekids.beans.stateful.CustomerBean;
+import planetekids.beans.stateful.CustomerRemote;
+
+@Validation
+public class CreateAction extends ActionSupport implements SessionAware {
+  
+    private Map session;
+    
+    private String emailAddress;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private int zipCode;
+    private String city;
+    private String phoneNumber;
+    private String faxNumber;
+    
+    public void setSession(Map session) {
+        this.session = session;
+    }
+    
+    private CustomerRemote getCustomer() {
+        return (CustomerRemote)session.get("customer");
+    }
+    
+    public String redirect() throws Exception {
+        session.put("content_action", "index_content");
+        session.put("content_namespace", "/account");
+        session.put("location_action", "index_location");
+        session.put("location_namespace", "/account");
+        return execute();
+    }
+    
+    @RequiredStringValidator(message="Supply e-mail address")
+    public String getEmailAddress() {
+	return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+	this.emailAddress = emailAddress;
+    }
+
+    @RequiredStringValidator(message="Supply password")
+    public String getPassword() {
+	return password;
+    }
+
+    public void setPassword(String password) {
+	this.password = password;
+    }
+
+    public String getFirstName() {
+	return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+	this.firstName = firstName;
+    }
+
+    public String getLastName() {
+	return lastName;
+    }
+
+    public void setLastName(String lastName) {
+	this.lastName = lastName;
+    }
+
+    public String getAddressLine1() {
+	return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+	this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+	return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+	this.addressLine2 = addressLine2;
+    }
+
+    public String getAddressLine3() {
+	return addressLine3;
+    }
+
+    public void setAddressLine3(String addressLine3) {
+	this.addressLine3 = addressLine3;
+    }
+
+    public int getZipCode() {
+	return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+	this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+	return city;
+    }
+
+    public void setCity(String city) {
+	this.city = city;
+    }
+
+    public String getPhoneNumber() {
+	return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+	this.phoneNumber = phoneNumber;
+    }
+
+    public String getFaxNumber() {
+	return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+	this.faxNumber = faxNumber;
+    }
+    
+    @Override
+    public String execute() throws Exception {
+        try {
+            if(getCustomer() == null) session.put("customer", new InitialContext().lookup(CustomerBean.class.getName() + "_" + CustomerRemote.class.getName() + "@Remote"));
+            return ActionSupport.SUCCESS;
+        } catch (NamingException ex) {
+            return ActionSupport.ERROR;
+        }
+    }
+}
--- trunk/web/account/create_content.jsp
+++ trunk/web/account/create_content.jsp
@@ -1,3 +1,7 @@
 <%@ page contentType="text/html; charset=UTF-8" %>
 <%@ taglib prefix="s" uri="/struts-tags" %>
 
+<s:form action="CreateAction" method="POST" validate="true">
+    
+    
+</s:form>
